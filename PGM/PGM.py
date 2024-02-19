ec544908bb561f8fb630f88be6992f041bbc0ced
AntoineCEA <antoine.hilberer@gmail.com>
2024-02-19 18:21:04 +0100

    added optional style sheet

--- PGM/PGM.py
+++ PGM/PGM.py
@@ -1,13 +1,17 @@
 import sys
 
 from PyQt5.QtWidgets import QApplication
-
+from pathlib import Path
 from main_ui import *
 
 # Run the application
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    app.setStyle('Fusion')
+    file = open("PGM/styles.qss",'r')
+
+    with file:
+	    qss = file.read()
+	    app.setStyleSheet(qss)
     window = MainWindow()
     window.show()
     sys.exit(app.exec_())
