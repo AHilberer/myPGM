908cf0dd38a1610301beb92d7af096a790869ad6
Antoine Hilberer <57757205+AHilberer@users.noreply.github.com>
2024-09-22 20:06:35 +0200

    Cleanup

--- myPGM/start.py
+++ myPGM/start.py
@@ -4,19 +4,18 @@ from PyQt5.QtWidgets import QApplication
 from main_ui import *
 
 # Run the application
-if __name__ == '__main__':
-
-    os.chdir(os.path.abspath(__file__).replace(os.path.basename(__file__), '') )
+if __name__ == "__main__":
+    os.chdir(os.path.abspath(__file__).replace(os.path.basename(__file__), ""))
     app = QApplication(sys.argv)
-    app.setStyle('Fusion')
+    app.setStyle("Fusion")
 
     try:
-        with open("light-mode.qss",'r') as file:
+        with open("light-mode.qss", "r") as file:
             qss = file.read()
             app.setStyleSheet(qss)
     except:
         pass
-    
+
     window = MainWindow()
     window.show()
     sys.exit(app.exec_())
