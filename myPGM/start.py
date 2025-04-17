eeca6c3e8bca5181a512d519f9156ea88d29a0cc
Antoine Hilberer <57757205+AHilberer@users.noreply.github.com>
2025-04-18 00:01:50 +0200

    small update

--- myPGM/start.py
+++ myPGM/start.py
@@ -2,6 +2,8 @@ import sys
 import os
 from PyQt5.QtWidgets import QApplication
 from main_ui import *
+from data_model import PressureGaugeDataManager
+from presenter import Presenter
 
 # Run the application
 if __name__ == "__main__":
@@ -15,7 +17,11 @@ if __name__ == "__main__":
             app.setStyleSheet(qss)
     except:
         pass
+    model = PressureGaugeDataManager()
+    view = MainWindow(model)
+    presenter = Presenter(model, view)
+    presenter.view.show()
+    presenter.initialize_example()
 
-    window = MainWindow()
-    window.show()
     sys.exit(app.exec_())
+
